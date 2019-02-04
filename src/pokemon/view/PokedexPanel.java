e9a8b33233c162186636160c12fb4f5634ce4bea
Luis Veliz <luisvelizjr@gmail.com>
2019-02-04 13:33:12 -0700

    Created  PokedexPanel and added setupPanel, setupLayout, setupListeners.

--- src/pokemon/view/PokedexPanel.java
+++ src/pokemon/view/PokedexPanel.java
@@ -1,6 +1,40 @@
 package pokemon.view;
 
-public class PokedexPanel
-{
+import javax.swing.*;
+import pokemon.controller.PokedexController;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Dimension;
+
+public class PokedexPanel extends JPanel
+{
+	private PokedexController appController;
+	
+	public PokedexPanel(PokedexController appController)
+	{
+		super();
+		
+		this.appController = appController;
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
 }
